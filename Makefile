404d26e419ca644f468df3e29e0619510470d960
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2026-03-05 21:06:29 +0300

    Release

---
+++ Makefile
@@ -0,0 +1,8 @@
+CC := g++
+CCF := -O1 -std=c++17
+CCLF := pkg-config --cflags --libs x11 xft fontconfig
+SRC := text.cpp
+OUT := font_shower
+
+main:
+	$(CC) $(CCF) $(SRC) -o $(OUT) `$(CCLF)`
---
+++ text.cpp
@@ -0,0 +1,70 @@
+#include <X11/Xlib.h>
+#include <fontconfig/fontconfig.h>
+#include <X11/Xft/Xft.h>
+#include <string>
+
+#define FontPath "/usr/share/fonts/truetype/cloister-black/CloisterBlack.ttf"
+const std::string Text = "Hello";
+
+
+int main() {
+    Display* MainDisplay = XOpenDisplay(0);
+    Window RootWindow = XDefaultRootWindow(MainDisplay);
+    
+    const int ScreenWidth = XDisplayWidth(MainDisplay, DefaultScreen(MainDisplay));
+    const int ScreenHeight = XDisplayHeight(MainDisplay, DefaultScreen(MainDisplay));
+    
+    const int WindowX = 0;
+    const int WindowY = 0;
+    const int WindowWidth = ScreenWidth;
+    const int WindowHeight = ScreenHeight;
+    const int BorderWidth = 0;
+    const int WindowDepth = CopyFromParent;
+    const int WindowClass = CopyFromParent;
+    Visual* WindowVisual = CopyFromParent;
+
+    const int AttributeValueMask = CWBackPixel | CWBorderPixel;
+    XSetWindowAttributes WindowAttributes = {};
+    WindowAttributes.background_pixel = 0xffffff;
+
+    Window MainWindow = XCreateWindow(MainDisplay, RootWindow, 
+            WindowX, WindowY, WindowWidth, WindowHeight,
+            BorderWidth, WindowDepth, WindowClass, WindowVisual,
+            AttributeValueMask, &WindowAttributes);
+
+    XSizeHints SizeHints; 
+    SizeHints.flags = PMinSize | PMaxSize;
+    SizeHints.min_width = WindowWidth;
+    SizeHints.max_width = WindowWidth;
+    SizeHints.min_height = WindowHeight;
+    SizeHints.max_height = WindowHeight; 
+    XSetWMNormalHints(MainDisplay, MainWindow, &SizeHints);
+    
+    XStoreName(MainDisplay, MainWindow, "Secret window");
+
+    XMapWindow(MainDisplay, MainWindow);
+    
+    FcPattern *Pattern = FcPatternBuild(0, FC_FILE, FcTypeString, FontPath, FC_PIXEL_SIZE, FcTypeDouble, 72.0, NULL);
+
+    XftFont *Font = XftFontOpenPattern(MainDisplay, Pattern);
+    XftDraw *Draw = XftDrawCreate(MainDisplay, MainWindow, DefaultVisual(MainDisplay, DefaultScreen(MainDisplay)), DefaultColormap(MainDisplay, DefaultScreen(MainDisplay)));
+    
+    XftColor TextColor;
+    XRenderColor RTextColor;
+    RTextColor.red = 0x0000;
+    RTextColor.green = 0x0000;
+    RTextColor.blue = 0x0000;
+    RTextColor.alpha = 0xffff;
+    XftColorAllocValue(MainDisplay, DefaultVisual(MainDisplay, DefaultScreen(MainDisplay)), DefaultColormap(MainDisplay, DefaultScreen(MainDisplay)), &RTextColor, &TextColor);
+
+    XftDrawStringUtf8(Draw, &TextColor, Font, ScreenWidth / 2.2, ScreenHeight / 2, reinterpret_cast<const XftChar8*>(Text.c_str()), Text.length());
+    while (true) {
+        XEvent GeneralEvent = {};
+        XNextEvent(MainDisplay, &GeneralEvent);
+    }
+
+    XftDrawDestroy(Draw);
+    XftFontClose(MainDisplay, Font);
+    XDestroyWindow(MainDisplay, MainWindow);
+    XCloseDisplay(MainDisplay);
+}
